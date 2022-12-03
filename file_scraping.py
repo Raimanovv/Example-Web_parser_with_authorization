da663aff11ce2c5b6201d073c09c6a74db930be7
Raimanovv <rustam.raimanovv@gmail.com>
2022-12-03 21:21:56 +0500

    0002 Fix the bugs. Work with one page

--- file_scraping.py
+++ file_scraping.py
@@ -10,19 +10,24 @@ work = Session()
 work.get("http://quotes.toscrape.com/", headers=headers)
 
 response = work.get("http://quotes.toscrape.com/login", headers=headers)
-
 soup = BeautifulSoup(response.text, 'lxml')
 
 token = soup.find('form').find('input').get('value')
-
 data = {'csrf_token': token, 'username': 'noname', 'password': 'password'}
-
 result = work.post("http://quotes.toscrape.com/login", headers=headers, data=data, allow_redirects=True)
 
-soup_home = BeautifulSoup(result.text, 'lxml')
-data_div_all = soup_home.find('div', class_='quote')
+response_page = work.get("http://quotes.toscrape.com/page/1/", headers=headers)
+soup_page = BeautifulSoup(response_page.text, 'lxml')
+data_div_all = soup_page.find_all('div', class_='quote')
 
-text = data_div_all.find('span', class_="text").text
-author = data_div_all.find('small', class_="author").text
 
-print(text + '\n' + author + '\n\n')
+def all_blog(list_blogs):
+    for i in list_blogs:
+        text = i.find('span', class_="text").text
+        author = i.find('small', class_="author").text
+        aa = i.find_all('a')[1].get('href')
+
+        print(text + '\n' + author + '\n' + aa + '\n\n')
+
+
+all_blog(data_div_all)
